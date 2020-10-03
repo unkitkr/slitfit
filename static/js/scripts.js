f5be87233cd1b3abc9511033b00b6b398431c8e1
Ankit Kumar <ankit.official.dev@gmail.com>
2020-10-04 01:34:13 +0530

    AJAX scrips added

---
+++ static/js/scripts.js
@@ -0,0 +1,114 @@
+
+
+$(document).ready(function() {
+	$('#createURLform').on('submit', function(event) {
+		$.ajax({
+			data : {
+				link_name : $('#link_name').val(),
+				long_link : $('#long_link').val()
+			},
+			type : 'POST',
+			url : '/app/createnewurl'
+		})
+		.done(function(data) {
+
+			if (data.error) {
+                document.getElementById('msg-box-ajax').style.display = 'block';
+                document.getElementById('ajax-response').innerHTML = data.error;
+			}
+			else {
+				document.getElementById('msg-box-ajax').style.display = 'block';
+				document.getElementById('ajax-response').innerHTML  = 'Url shortended and is available at: ' + `<a href = '${data.url}'>${data.url}</a>`;
+			}
+
+        });
+        event.preventDefault();
+    });
+    
+
+    $('.explicit-link-delete').on('click', function(event) {
+		$.ajax({
+			data : {
+				link_name : $('.explicit-link-delete').attr('id'),
+			},
+			type : 'POST',
+			url : '/app/deletelink/'+ $('.explicit-link-delete').attr('id')
+		})
+		.done(function(data) {
+
+			if (data.error) {
+                document.getElementById('msg-box-ajax').style.display = 'block';
+                document.getElementById('ajax-response').innerHTML = data.error;
+			}
+			else {
+				document.getElementById('msg-box-ajax').style.display = 'block';
+				document.getElementById('ajax-response').innerHTML  = data.success;
+			}
+
+        });
+        event.preventDefault();
+    });
+    
+
+    $('.explicit-link-deactive').on('click', function(event) {
+		$.ajax({
+			data : {
+				link_name : $('.explicit-link-deactive').attr('id'),
+			},
+			type : 'POST',
+			url : '/app/delink/'+ $('.explicit-link-deactive').attr('id')
+		})
+		.done(function(data) {
+
+			if (data.error) {
+                document.getElementById('msg-box-ajax').style.display = 'block';
+                document.getElementById('ajax-response').innerHTML = data.error;
+			}
+			else {
+				document.getElementById('msg-box-ajax').style.display = 'block';
+				document.getElementById('ajax-response').innerHTML  = data.success;
+			}
+
+        });
+        event.preventDefault();
+    });
+    
+
+
+
+    $('.explicit-link-reactive').on('click', function(event) {
+		$.ajax({
+			data : {
+				link_name : $('.explicit-link-reactive').attr('id'),
+			},
+			type : 'POST',
+			url : '/app/reactivatelink/'+ $('.explicit-link-reactive').attr('id')
+		})
+		.done(function(data) {
+
+			if (data.error) {
+                document.getElementById('msg-box-ajax').style.display = 'block';
+                document.getElementById('ajax-response').innerHTML = data.error;
+			}
+			else {
+				document.getElementById('msg-box-ajax').style.display = 'block';
+				document.getElementById('ajax-response').innerHTML  = data.success;
+			}
+
+        });
+        event.preventDefault();
+	});
+
+
+
+});
+
+function hide_msg() {
+    document.getElementById('msg-box').style.display = 'none';
+    console.log('sdsd');
+}
+
+function hide_msg() {
+    document.getElementById('msg-box-ajax').style.display = 'none';
+    console.log('sdsd');
+}
