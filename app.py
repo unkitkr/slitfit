e32febb8a6047d361a1caea8aa05f11c92b412d2
Ankit Kumar <ankit.official.dev@gmail.com>
2020-10-05 16:53:06 +0530

    removed slitfit url

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def create_new_url():
             try:
                 db.session.add(new_short_url)
                 db.session.commit()
-                redirect_url = "www.slit.fit/"+hashed_url
+                redirect_url = "/"+hashed_url
                 return jsonify({
                     'url': redirect_url,
                 })
